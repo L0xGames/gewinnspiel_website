ac8b70ef29bd8ed256b9d0a590658d1bf93438f9
Kian <kiandousti@yahoo.de>
2022-04-06 21:32:18 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Navbar, Container } from "react-bootstrap";
+import { SocialIcon } from "react-social-icons";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`The name you entered was: ${name}`);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload!!joo
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar>
+        <Container>
+          <Navbar.Brand href="#home">Kian</Navbar.Brand>
+          <div>
+            <SocialIcon
+              style={{ margin: 3 }}
+              url="https://www.instagram.com/kian_d/"
+            />
+            <SocialIcon
+              style={{ margin: 3 }}
+              url="https://www.tiktok.com/@kiandousti"
+            />
+            <SocialIcon
+              style={{ margin: 3 }}
+              url="https://www.youtube.com/channel/UCembxrqzbOcTOlm3NMZiz3A"
+            />
+          </div>
+        </Container>
+      </Navbar>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <form onSubmit={handleSubmit}>
+          <label>Gib die Lösung vom letzten Video ein:</label>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input type="submit" />
+        </form>
+      </div>
+    </>
   );
 }
 
